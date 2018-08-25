0ece74f59ab89585752bc4db8334e2e2171571d8
Budh Ram Gurung <budhram.gurung01@gmail.com>
2018-08-25 16:43:00 +0530

    Added subject and created its link at top nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140224203311) do
+ActiveRecord::Schema.define(version: 20180825110247) do
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/subjects.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :subject do
+    name "MyString"
+  end
+end
