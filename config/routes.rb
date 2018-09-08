5ba82130dfb8ca408b1b5d76aabaa9665c58f8f6
nisusam <nishant.samel@gmail.com>
2018-09-08 16:07:10 +0530

    Follow `Deep Nesting` in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+  resources :subjects do
+    resources :books
+  end
+
+  resources :books do
+    resources :chapters
+  end
+
+  resources :chapters do
+    resources :topics
+  end
+
+  resources :topics do
+    resources :sub_topics
+  end
+
   resources :sub_topics
-  resources :topics
-  resources :chapters
-  resources :books
-  resources :subjects
+
   root to: 'visitors#index'
   devise_for :users
   resources :users
